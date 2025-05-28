fc7dbf538b2e093d5f1b2ca4ca2f716a820f7573
dharmveer97 <dvbangar8@gmail.com>
2025-05-28 20:37:05 +0530

    removed extra plugins

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,8 +1,12 @@
+"use client";
+import React from "react";
+
 import Image from "next/image";
+import config from "@/utils/config";
 
 const Logo = () => (
   <div>
-    <Image src="/images/logo.png" width={100} alt="TheKickoffretreat" />
+    <Image src="/images/logo.png" width={100} alt={config.siteName} />
   </div>
 );
 
